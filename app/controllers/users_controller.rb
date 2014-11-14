6af434ee43dca79c7233b848de8b0fe7985fbb31
getaclue <dropbyalex@gmail.com>
2014-11-14 12:32:57 +0000

    Added a sign up process

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,40 @@
 class UsersController < ApplicationController
+  
+  # Responds to GET request
+  # Purpose - Page to make a new user
+  # Named path - new_user_path
   def new
+    @user = User.new
   end
+  
+  # Responds to GET request
+  # Purpose - Page to show user
+  # Named path - user_path(user)
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  # Responds to POST request
+  # Purpose - Create a new user
+  # Named path - users_path
+  def create
+    @user = User.new(accepted_user_params)
+    if @user.save
+      # Successfull Sign Up
+      # Redirect to showing user profile
+      redirect_to user_path(@user)
+    else
+      render 'new'
+    end
+  end
+  
+  # Helper methods
+  private
+  
+    # Ensure that only the permitted parameters are passed and no others
+    def accepted_user_params
+      params.require(:user).permit(:name, :email, :age, :sex, :password, 
+                                    :password_confirmation)
+    end
+  
 end
