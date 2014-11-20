ac0458c78d5350fc053e927271c625fe7c6fe6c8
getaclue <dropbyalex@gmail.com>
2014-11-20 23:27:55 +0000

    Added a way to update the survey title.

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,7 +1,7 @@
 class SurveysController < ApplicationController
   # make sure user is signed in
   # user can only create a survey and destroy a survey
-  before_action :signed_in_user, only: [:create, :destroy, :show]
+  before_action :signed_in_user, only: [:create, :destroy, :show, :update]
   
   # show contents of a survey
   def show
@@ -9,6 +9,17 @@ class SurveysController < ApplicationController
     # redirect to root if the survey doesn't exist
     if @survey.nil?
       redirect_to root_url
+    end
+  end
+  
+  # update survey title
+  def update
+    @survey = current_user.surveys.find_by(id: params[:id])
+    if @survey.update_attributes(survey_parameters)
+      flash[:success] = "Title changed"
+      redirect_to @survey
+    else
+      render 'show'
     end
   end
   
@@ -37,5 +48,5 @@ class SurveysController < ApplicationController
     def survey_parameters
       params.require(:survey).permit(:title)
     end
-  
+    
 end
