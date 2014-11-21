01c2a1d34c0c9ff7f69c45d81a20430c1935a10f
getaclue <dropbyalex@gmail.com>
2014-11-21 01:17:13 +0000

    Removed question model. Will rename to simply SurveyItem.

--- app/models/question.rb
+++
@@ -1,8 +0,0 @@
-class Question < ActiveRecord::Base
-  belongs_to :survey
-  
-  # make sure a question is associated with a survey via
-  # survey_id
-  validates :survey_id, presence: true
-  validates :content, presence: true
-end
