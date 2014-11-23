d48fe6141a853345a540f08f1bd68e661ca1bf5d
getaclue <dropbyalex@gmail.com>
2014-11-23 06:08:56 +0000

    Can now add, and edit answers, and question title from the same page.

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -28,6 +28,13 @@ class AnswersController < ApplicationController
     end
   end
   
+  def update
+    answer_to_update = current_user.surveys.find(current_survey.id).questions.find(params[:question_id]).answers.find(params[:id])
+    answer_to_update.update_attributes(create_new_survey_item_parameters)
+    flash[:success] = "Edited an answer"
+    redirect_to question_path(parent_question)
+  end
+  
   private
     # used to create an answer and a quesiton
     def create_new_survey_item_parameters
