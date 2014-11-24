3eacac1d32ea853fb6987aea123439cc377afd7f
getaclue <dropbyalex@gmail.com>
2014-11-24 19:05:47 +0000

    Adding commentary. Last minute changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,11 @@
 class PagesController < ApplicationController
+  #################################################################
+  # Responsible for showing the main page of the application
+  #################################################################
   def index
+    #################################################################
+    # Create an instance of a survey only if the user is signed in
+    #################################################################
     @survey = current_user.surveys.build if signed_in?
   end
 end
